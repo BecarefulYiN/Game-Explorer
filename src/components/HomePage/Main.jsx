8f733db150e0bb87b08d61a0e08b0421849113e8
Wai Yan Oo <waiyanoko37@gmail.com>
2024-04-06 00:19:44 +0630

    Homepage2

--- src/components/HomePage/Main.jsx
+++ src/components/HomePage/Main.jsx
@@ -1,16 +1,28 @@
-import { Box } from '@mui/material'
-import React from 'react'
+import { Box, Card } from '@mui/material';
+import React from 'react';
+import CardMedia from '@mui/material/CardMedia';
+import Valorant from '../../assects/Images/Valorant.jpg';
+import PromoteSlide from './PromoteSlide';
 
 function Main() {
   return (
-    <Box sx={{
-      width: '85vw',
-      height: '100vh',
-      backgroundColor: 'pink'
-    }}>
-
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        width: '85vw',
+        height: 'auto',
+        padding: '20px',
+        gap: '30px'
+      }}
+    >
+        <PromoteSlide />
+      <Card sx={{ maxWidth: 300 }}>
+        <CardMedia component="img" height="200" image={Valorant} />
+      </Card>
     </Box>
-  )
+  );
 }
 
-export default Main
+export default Main;
---
+++ src/components/HomePage/PromoteSlide.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Box, Card, CardMedia, Typography } from '@mui/material';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Valorant from '../../assects/Images/Valorant.jpg';
+import PromotedGame from '../../assects/Uplodes/PromotedGame1.jpg'
+
+const images = [
+  {
+    src: PromotedGame,
+    alt: 'Image 1',
+  },
+  {
+    src: PromotedGame,
+    alt: 'Image 2',
+  },
+];
+
+function PromoteSlide() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000, // 2 seconds
+    nextArrow: false, 
+    prevArrow: false, 
+  };
+
+  return (
+    <Box sx={{ width: '70vw', height: 'auto'}}>
+      <Slider {...settings}>
+        {images.map((image, index) => (
+          <Card key={index}>  
+            <CardMedia
+              component="img"
+              height="300"
+              image={image.src}
+              alt={image.alt}
+            />
+          </Card>
+        ))}
+      </Slider>
+    </Box>
+  );
+}
+
+export default PromoteSlide;
